daef1b34b0d3e4475e1f579e0be562a75efce28c
Yousef Meska <42917814+MrBomber0x001@users.noreply.github.com>
2022-06-02 13:09:31 +0200

    Time: 332 ms (16.44%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,16 +1,13 @@
-# Write your MySQL query statement below
-select 
-IFNULL(
-    (SELECT distinct salary from Employee
-order by salary desc
-limit 1 offset 1), null) as SecondHighestSalary;
 
-# select (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1) As SecondHighestSalary
+WITH CTE AS 
+(
+    SELECT Salary,
+    dense_rank() over (ORDER BY Salary Desc) as Dr
+    FROM Employee
+)
 
-
-# SELECT IFNULL((SELECT salary FROM Employee
-# where salary < (select MAX(salary) FROM Employee)
-# order by salary desc 
-# limit 1), null) as SecondHighestSalary;
-
-
+SELECT 
+CASE 
+    WHEN MAX(CTE.dr) < 2 THEN null ELSE Salary
+END AS SecondHighestSalary
+FROM CTE WHERE Dr = 2
